434b6098130304c3b5e13c138e6469410fab2925
Robertus Lilik Haryanto <rharyant@redhat.com>
2022-12-12 21:53:15 +0800

    Removed unused files and outputFile on shade plugin

--- src/container/resources/app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Go to application directory
-cd "$(dirname "$0")"
-
-# Run application
-# java $JAVA_OPTS -Dapp=$APP_NAME -jar /opt/$APP_NAME/$APP_NAME-$APP_VERSION.jar
-java $JAVA_OPTS -Dapp=$APP_NAME -jar $APP_NAME*.jar
